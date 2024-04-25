cb5e824c854f6951d96f50ed7216cfe60dbc4363
Daniel Baluta <daniel.baluta@nxp.com>
2024-04-25 09:00:52 +0300

    lab08: ex 1: Add solutions for ex1
    We use push and pop to interchange values of eax and ebx registers.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- laborator/content/stiva/1-max/max.asm
+++ laborator/content/stiva/1-max/max.asm
@@ -9,8 +9,17 @@ main:
     mov eax, 1
     mov ebx, 4 
 
-    ; TODO: get maximum value. You are only allowed to use one conditional jump and push/pop instructions.
+    cmp eax, ebx
+    ja out
 
+    push eax
+    push ebx
+    pop eax
+    pop ebx
+
+
+    ; TODO: get maximum value. You are only allowed to use one conditional jump and push/pop instructions.
+out:
     PRINTF32 `Max value is: %d\n\x0`, eax ; print maximum value
 
     ret
